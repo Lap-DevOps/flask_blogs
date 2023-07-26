332e767750bc047a0cd6981e59f913f164366fc5
AndriiLevin <andriilevin@gmail.com>
2023-07-26 21:22:35 +0100

    -- add admin_base.html

--- admin/admin.py
+++ admin/admin.py
@@ -3,38 +3,45 @@ from flask import Blueprint, request, redirect, url_for, flash, render_template,
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
 
 
-def login_admin():
-    session["admin_ligged"] = 1
-
-
-@admin.route("/")
-def index():
-    return "admin"
-
+menu = [{'url': '.index', 'title': 'Panel'},
+        {'url': '.logout', 'title': 'Log out'}]
 
 def isLogged():
-    return True if session.get("admin_lagged") else False
+    return True if session.get('admin_logged') else False
 
+def login_admin():
+    session['admin_logged'] = 1
 
 def logout_admin():
     session.pop('admin_logged', None)
+
+@admin.route('/')
+def index():
+    if not isLogged():
+        return redirect(url_for('.login'))
+
+    return render_template('admin/index.html', menu=menu, title='Admin panel')
+
+@admin.route('/login', methods=["POST", "GET"])
+def login():
+    if isLogged():
+        return redirect(url_for('.index'))
+
+    if request.method == "POST":
+        if request.form['user'] == "admin" and request.form['psw'] == "12345":
+            login_admin()
+            return redirect(url_for('.index'))
+        else:
+            flash("Неверная пара логин/пароль", "error")
+
+    return render_template('admin/login.html', title='Admin panel')
 
 
 @admin.route('/logout', methods=["POST", "GET"])
 def logout():
     if not isLogged():
         return redirect(url_for('.login'))
+
     logout_admin()
 
     return redirect(url_for('.login'))
-
-
-@admin.route('/login', methods=["POST", "GET"])
-def login():
-    if request.method == 'POST':
-        if request.form['user'] == 'admin' and request.form['123']:
-            login_admin()
-            return redirect(url_for('.index'))
-        else:
-            flash('Not correct login/password')
-    return render_template('admin/login.html', title='Admin panel')
