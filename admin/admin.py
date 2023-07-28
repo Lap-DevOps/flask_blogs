d4b4c7230d8e38a5e1253d3ddfc515860c63b31a
AndriiLevin <andriilevin@gmail.com>
2023-07-28 19:03:47 +0100

    -- add publiction list

--- admin/admin.py
+++ admin/admin.py
@@ -1,19 +1,39 @@
-from flask import Blueprint, request, redirect, url_for, flash, render_template, session
+import sqlite3
+
+from flask import Blueprint, request, redirect, url_for, flash, render_template, session, g
 
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
-
 
 menu = [{'url': '.index', 'title': 'Panel'},
         {'url': '.logout', 'title': 'Log out'}]
 
+db = None
+
+
+@admin.before_request
+def before_request():
+    global db
+    db = g.get('link_db')
+
+
+@admin.teardown_request
+def teaerdown_request(request):
+    global db
+    db = None
+    return request
+
+
 def isLogged():
     return True if session.get('admin_logged') else False
+
 
 def login_admin():
     session['admin_logged'] = 1
 
+
 def logout_admin():
     session.pop('admin_logged', None)
+
 
 @admin.route('/')
 def index():
@@ -21,6 +41,7 @@ def index():
         return redirect(url_for('.login'))
 
     return render_template('admin/index.html', menu=menu, title='Admin panel')
+
 
 @admin.route('/login', methods=["POST", "GET"])
 def login():
@@ -45,3 +66,21 @@ def logout():
     logout_admin()
 
     return redirect(url_for('.login'))
+
+
+@admin.route("/list-pubs")
+def list_pubs():
+    if not isLogged():
+        return redirect(url_for('.login'))
+
+    list = []
+
+    if db:
+        try:
+            cur = db.cursor()
+            cur.execute(f"SELECT title, text, url FROM posts")
+            list = cur.fetchall()
+        except sqlite3.Error as e:
+            print("Error read data" + str(e))
+
+    return render_template("admin/listpubs.html", title="Publication list", menu=menu, list=list)
